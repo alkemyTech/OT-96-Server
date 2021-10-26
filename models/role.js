2f7ebcbc9b2ffb6542a63770c5c7fbc523505d88
Aleex1237 <alexis10893@outlook.com>
2021-10-26 11:18:43 -0300

    added association with users in the role.js model

--- models/role.js
+++ models/role.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Role.hasMany(models.User, {as:"users"})
     }
   };
   Role.init({
