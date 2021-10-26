95bc23ee946c7d19dfe997d621965715485e2584
Aleex1237 <alexis10893@outlook.com>
2021-10-26 16:16:25 -0300

    added file rolesControllers and getAll & getById funcionality

---
+++ controllers/rolesController.js
@@ -0,0 +1,39 @@
+const db = require("../models");
+
+module.exports = {
+  getAll: async (req, res) => {
+    try {
+      let all = await db.Role.findAll();
+      if (data.length == 0) {
+        return res.status(404).json({
+          status: 404,
+          msg: `No existe el rol con ID: ${req.params.id}`,
+        });
+      }
+      return res
+        .status(200)
+        .json({ status: 200, length: all.length, data: all });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ status: 500, msg: error });
+    }
+  },
+  getById: async (req, res) => {
+    try {
+      let one = await db.Role.findByPk(req.params.id, {
+        include: [{ association: "users" }],
+      });
+
+      if (!one) {
+        return res.status(404).json({
+          status: 404,
+          msg: `No existe el rol con ID: ${req.params.id}`,
+        });
+      }
+      return res.status(200).json({ status: 200, data: one });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ status: 500, msg: error });
+    }
+  },
+};
