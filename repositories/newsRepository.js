9f29809246c20a3a0465c736edfd6cb5d4492d6a
Federico Palmieri <fedepalmie95@gmail.com>
2021-10-27 01:21:31 -0300

    adding repositories/newsRepository.js

---
+++ repositories/newsRepository.js
@@ -0,0 +1,17 @@
+const db = require('../models');
+
+module.exports.getById = async ( id ) => {
+    
+};
+
+module.exports.create = async () => {
+
+}
+
+module.exports.update = async () => {
+
+}
+
+module.exports.delete = async () => {
+
+}
