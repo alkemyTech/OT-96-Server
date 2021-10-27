ef612f56ab03905fe8375576e0084d7b9f6ff7aa
juancruz_94_ <juancruz_94_@hotmail.com>
2021-10-27 18:08:36 -0300

    testimonial repository

---
+++ repositories/testimonial.js
@@ -0,0 +1,43 @@
+const Testimonial = require('../models').testimonial;
+
+// get all testimonials
+async function getAll() {
+  const response = await Testimonial.findAll();
+  return response;
+}
+
+// get one testimonial by id
+async function getById(id) {
+  const response = await Testimonial.findOne({
+    where: { id: id }
+  });
+  return response;
+}
+
+// create testimonial
+async function create(data) {
+  const response = await Testimonial.create({
+    name: data.name,
+    image: data.image,
+    content: data.address,
+  });
+  return response;
+}
+
+// update testimonial
+async function update(id, data) {
+  const response = await Testimonial.update(data, {
+    where: { id: id }
+  });
+  return response;
+}
+
+// delete testimonial
+async function deleteById(id) {
+  const response = await Testimonial.destroy({
+    where: { id: id }
+  });
+  return response;
+}
+
+module.exports = { getAll, getById, create, update, deleteById };
