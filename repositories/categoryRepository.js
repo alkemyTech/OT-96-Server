7aad43179d4cf53672743d6cd316bb0caa58b42c
Agustin <agualvila@gmail.com>
2021-10-27 09:34:31 -0300

    add basic BD operations in categoryRepository

--- repositories/categoryRepository.js
+++ repositories/categoryRepository.js
@@ -1,6 +1,6 @@
-const Category = require('../models/category');
+const { Category } = require('../models/category');
 
-// example:
+// Get all categories
 const getAll = async () => {
     const response = await Category.findAll({
         attributes: {
@@ -11,6 +11,46 @@ const getAll = async () => {
     return response;
 }
 
+// Get one category by id
+const getById = async (id) => {
+    const response = await Category.findByPk(id);
+
+    return response;
+}
+
+// Insert new category
+const create = async (data) => {
+    const response = await Category.create(data);
+
+    return response;
+}
+
+// Update category
+const update = async (id, data) => {
+    const response = await Category.update(data, {
+        where: {
+            id: id
+        }
+    });
+
+    return response;
+}
+
+// Delete one category by id
+const deleteById = async (id) => {
+    const response = await Category.destroy({
+        where: {
+            id: id
+        }
+    });
+
+    return response;
+}
+
 module.exports = {
-    getAll
+    getAll,
+    getById,
+    create,
+    update,
+    deleteById
 }
