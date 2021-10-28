20dbf1b012fb4d983f35dc218dd305b3a1a0beda
Federico Palmieri <fedepalmie95@gmail.com>
2021-10-28 13:21:35 -0300

    modified: middleware to verify admin user

--- middlewares/auths.js
+++ middlewares/auths.js
@@ -3,7 +3,16 @@ const usersService = require('../services/users')
 
 module.exports.isAdmin = async (req, res, next) => {
 
-    const userId = authsService.verifyToken();
+    const header = req.headers['authorization'];
+
+    if (typeof header === 'undefined') {
+        req.status(403).json({ message: 'header is undefined' });
+        return;
+    }
+    const bearer = header.split(' ');
+    const token = bearer[1];
+
+    const userId = authsService.verifyToken(token);
     const user = usersService.getById(userId);
 
     if (user.roleId !== 1) {
