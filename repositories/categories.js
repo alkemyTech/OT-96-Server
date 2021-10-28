cde1d481fd6102b894c192b14f8cd8f62f90d8f9
Agustín Avila Humerez <agualvila@gmail.com>
2021-10-28 20:42:50 -0300

    Config categories (#3)
    * Categories: migration, model, repository, service, controller

---
+++ repositories/categories.js
@@ -0,0 +1,56 @@
+const { Category } = require('../models/category');
+
+// Get all categories
+const getAll = async () => {
+    const response = await Category.findAll({
+        attributes: {
+            exclude: ['description', 'deletedAt', 'createdAt', 'updatedAt'],
+        }
+    });
+
+    return response;
+}
+
+// Get one category by id
+const getById = async (id) => {
+    const response = await Category.findByPk(id);
+
+    return response;
+}
+
+// Insert new category
+const create = async (data) => {
+    const response = await Category.create(data);
+
+    return response;
+}
+
+// Update category
+const update = async (id, data) => {
+    const response = await Category.update(data, {
+        where: {
+            id: id
+        }
+    });
+
+    return response;
+}
+
+// Remove one category by id
+const remove = async (id) => {
+    const response = await Category.destroy({
+        where: {
+            id: id
+        }
+    });
+
+    return response;
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+}
---
+++ services/categories.js
@@ -0,0 +1,12 @@
+const categoriesRepository = require('../repositories/categories');
+
+//example:
+/**
+ const getAll = async () => {
+        return  await categoriesRepository.getAll();
+    }
+
+module.exports = {
+    getAll
+    }
+ */
