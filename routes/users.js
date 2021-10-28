4202d97c2effc060355add4bf52c06c3d68425f9
ivan gabriel achocalla <ivangabriel.2048@gmail.com>
2021-10-28 19:22:27 -0300

    update the path of 'routes / users' to that of the current user controller

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 const userRouter = Router();
-const userController = require('../controllers/userControllers')
+const usersController = require('../controllers/users')
 
 /* GET users listing. */
-userRouter.get('/', userController.getAllUser);
-userRouter.get('/:id', userController.getUser);
-userRouter.post('/', userController.createUser);
-userRouter.put('/:id', userController.updateUser);
-userRouter.delete('/:id', userController.deleteUser);
+userRouter.get('/', usersController.getAllUser);
+userRouter.get('/:id', usersController.getUser);
+userRouter.post('/', usersController.createUser);
+userRouter.put('/:id', usersController.updateUser);
+userRouter.delete('/:id', usersController.deleteUser);
 
 module.exports = userRouter;
