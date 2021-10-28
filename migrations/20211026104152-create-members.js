e77509eef0dcad722ee3b608305fde733e7fc93c
Francisco Olivero Sagasta <francisco.olivero1998@gmail.com>
2021-10-28 15:46:52 -0300

    model, controller and migrations Member

---
+++ migrations/20211026104152-create-members.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Members', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      facebookUrl: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      instagramUrl: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      linkedinUrl: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      descriptcion: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+
+    }, );
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Members');
+  }
+};
