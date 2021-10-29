7f8ef14dd58d84b51fff114a16c24d0884415b2f
Federico Palmieri <fedepalmie95@gmail.com>
2021-10-29 00:45:38 -0300

    fixed to isAdmin Middleware

--- middlewares/auths.js
+++ middlewares/auths.js
@@ -2,28 +2,31 @@ const securityService = require('../services/security');
 const usersService = require('../services/users')
 
 module.exports.isAdmin = async (req, res, next) => {
+    try {
+        const token = req.headers['authorization'];
 
-    const token = req.headers['authorization'];
+        if (!token) {
+            res.status(403).json({ message: 'No token provided' });
+            return;
+        }
 
-    if (!token) {
-        res.status(403).json({ message: 'No token provided' });
-        return;
+        const userId = securityService.verifyToken(token).id;
+        if(!userId) {
+            res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        const user = await usersService.getById(userId);
+        if (!character) {
+            res.status(404).json({ message: 'no user found' });
+            return;
+        }
+
+        if (user.roleId !== 1) {
+            req.status(403).json({ message: 'Require Admin role' });
+            return;
+        }
+        next();
+    } catch(error) {
+        next(error);
     }
-    
-    const userId = securityService.verifyToken(token).id;
-    if(!userId) {
-        res.status(401).json({ message: 'Unauthorized' });
-    }
-
-    const user = usersService.getById(userId);
-    if (!character) {
-        res.status(404).json({ message: 'no user found' });
-        return;
-      }
-
-    if (user.roleId !== 1) {
-        req.status(403).json({ message: 'Require Admin role' });
-        return;
-    }
-    next();
 }
