073abdc5053b574a02350c428820df3b193b96af
Preiti Matias <73038375+MatiasPreiti@users.noreply.github.com>
2021-10-29 13:39:50 -0300

    Organization model/ot96 117 (#2)
    * create Model-Migration-Controller of Organization
    
    * repositories + softDeleted + refactor
    
    * Service implementation + conventions
    
    * bug fix + corrections
    
    * Model - bug fix

---
+++ repositories/organizations.js
@@ -0,0 +1,62 @@
+const { Organization } = require('../models/organization');
+
+//repository organization
+
+async function getAll() {
+  const organization = await Organization.findAll();
+  return organization;
+}
+
+async function getById(id) {
+  const organization = await Organization.findOne({
+    where: {
+      id: id,
+    },
+  });
+  return organization;
+}
+
+async function create(data) {
+  const organization = await Organization.create({
+    name: data.name,
+    image: data.image,
+    address: data.address,
+    phone: data.phone,
+    email: data.email,
+    welcomeText: data.welcomeText,
+    aboutUsText: data.aboutUsText,
+  });
+  return organization;
+}
+
+async function update(id, data) {
+  const values = {
+    name: data.name,
+    image: data.image,
+    address: data.address,
+    phone: data.phone,
+    email: data.email,
+    welcomeText: data.welcomeText,
+    aboutUsText: data.aboutUsText,
+  };
+  const condition = {
+    where: {
+      id: id,
+    },
+  };
+  const organization = await Organization.update(values, {
+    ...condition,
+  });
+  return organization;
+}
+
+async function remove(id) {
+  const organization = await Organization.destroy({
+    where: {
+      id: id,
+    },
+  });
+  return organization;
+}
+
+module.exports = { getAll, getById, create, update, remove };
