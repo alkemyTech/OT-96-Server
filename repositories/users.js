fadf80e7f3964b8cf9004f87ba6f96442b6c5ad9
Matias Preiti <preitimatias@gmail.com>
2021-10-28 21:06:01 -0300

    bug fix error users

--- repositories/users.js
+++ repositories/users.js
@@ -1,7 +1,7 @@
-const usersModel = require('../models').User;
+const { User } = require('../models.users');
 
 async function create(userData) {
-  const user = await usersModel.create(userData);
+  const user = await User.create(userData);
   return user;
 }
 
