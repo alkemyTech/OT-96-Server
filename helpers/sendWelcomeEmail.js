d4771bd0842f8af1d4c93ee06b7d8e33a9e85b53
juancruz_94_ <juancruz_94_@hotmail.com>
2021-10-29 17:01:50 -0300

    refactor

--- helpers/sendWelcomeEmail.js
+++ helpers/sendWelcomeEmail.js
@@ -1,12 +1,12 @@
 // require sendEmail
-const sendEmail = require('./helpers/sendEmail');
+const emailSender = require('./services/emailSender');
 
-const organizationsController = require('./controllers/organizations');
+const organizationsService = require('./services/organizations');
 
 // Send a welcome mail to an email given
 export function sendWelcomeEmail(email, organizationId) {
   const organization = organizationsController.getById(organizationId);
   let subject = "Welcome to " + organization.name;
   let html = ejs.render('welcomeEmail', {organization: this.organization});
-  sendEmail(email, subject, html);
+  emailSender(email, subject, html);
 }
