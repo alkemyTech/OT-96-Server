e16932c59ffdd237336d4bd915fb094cbe619834
Federico Palmieri <fedepalmie95@gmail.com>
2021-10-28 23:17:11 -0300

    update isAdmin Middleware

--- middlewares/auths.js
+++ middlewares/auths.js
@@ -11,7 +11,15 @@ module.exports.isAdmin = async (req, res, next) => {
     }
     
     const userId = securityService.verifyToken(token).id;
+    if(!userId) {
+        res.status(401).json({ message: 'Unauthorized' });
+    }
+
     const user = usersService.getById(userId);
+    if (!character) {
+        res.status(404).json({ message: 'no user found' });
+        return;
+      }
 
     if (user.roleId !== 1) {
         req.status(403).json({ message: 'Require Admin role' });
