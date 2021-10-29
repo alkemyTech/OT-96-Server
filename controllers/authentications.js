2533238b803960ec0de5931cba39f72d39e1eeda
ivan gabriel achocalla <ivangabriel.2048@gmail.com>
2021-10-29 00:00:56 -0300

    renaming of 'auth' files

--- controllers/authentications.js
+++ controllers/authentications.js
@@ -1,20 +1,20 @@
-const { validateLoginDetails, comparePasswords, existEmailUser, createToken } = require('../services/authService');
+const authenticationsService = require('../services/authentications')
 
 const loginUser = async (req, res) => {
   try {
-    const { error } = validateLoginDetails(req.body)
+    const { error } = authenticationsService.validateLoginDetails(req.body)
     if (error) return res.status(400).json({ ok:false, message:'validation false'})
     const { email, password } = req.body
-    const existingUser = await existEmailUser(email)
+    const existingUser = await authenticationsService.existEmailUser(email)
     if (!existingUser) return res.status(400).json({ ok:false , message:'email dont exist'})
     
-    const match = comparePasswords(password, existingUser.dataValues.password)
+    const match = authenticationsService.comparePasswords(password, existingUser.dataValues.password)
 
     if (match) {
       const { id, firstName, lastName, email, roleId } = existingUser.dataValues
       const user = { id, firstName, lastName, email, roleId }
       
-      const token = createToken({firstName, email, roleId})
+      const token = authenticationsService.createToken({firstName, email, roleId})
       res.status(200).json({
         accessToken: `Bearer ${token}`,
         user,
