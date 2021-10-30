b4269a46dbb50ce01ff1e88626ce348044df4338
JCAlmazan <juancruz_94_@hotmail.com>
2021-10-29 21:13:58 -0300

    testimonials seeder (#27)

---
+++ seeders/20211029220548-create-demo-testimonials.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('testimonials', [{
+      name: 'testimonio1',
+      image: 'https://i.ytimg.com/vi/R-Oz2g3lo_E/maxresdefault.jpg',
+      content: 'Estoy muy agradecido',
+      createdAt: new Date,
+      updatedAt: new Date
+    },
+    {
+      name: 'testimonio2',
+      image: 'https://significado.net/wp-content/uploads/2015/03/Testimoniar-es-declarar-algo.jpg',
+      content: 'Esta ONG me sirvió mucho',
+      createdAt: new Date,
+      updatedAt: new Date
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('testimonials', null, {});
+  }
+};
