e3599f031d910008340449676a405944f39479a3
Aleex1237 <alexis10893@outlook.com>
2021-11-01 20:58:18 -0300

    emailSender refact & new Api Key

--- services/emailSender.js
+++ services/emailSender.js
@@ -1,21 +1,21 @@
-const sendgridMail = require('@sendgrid/mail');
+const sendgridMail = require("@sendgrid/mail");
 sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
-const emailSender = (email, subject, text, html) => {
-  const msg = {
-    to: email,
-    from: 'alexis10893123@hotmail.com',
-    subject: subject,
-    text: text,
-    html: html,
-  };
-  sendgridMail
-    .send(msg)
-    .then(() => {
-      console.log({ msg: 'Email sent successfully!' });
-    })
-    .catch((error) => {
-      console.error({ error: error, msg: 'Email sent failed' });
-    });
+const emailSender = async (email, subject, text, html) => {
+  try {
+    const msg = {
+      to: email,
+      from: "alexis10893123@hotmail.com",
+      subject: subject,
+      text: text,
+      html: html,
+    };
+
+    const result = await sendgridMail.send(msg);
+    console.log(result);
+    console.log({ msg: "Email sent successfully!" });
+  } catch (error) {
+    console.error({ error: error, msg: "Email sent failed" });
+  }
 };
 
 module.exports = { emailSender };
