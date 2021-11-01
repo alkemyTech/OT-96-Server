c7b75dd2c98ef385b2360432b4269d633f46eaec
Aleex1237 <alexis10893@outlook.com>
2021-11-01 20:19:37 -0300

    fix sendWelcome Email

--- helpers/sendWelcomeEmail.js
+++ helpers/sendWelcomeEmail.js
@@ -1,11 +1,13 @@
 // require sendEmail
 const { emailSender } = require('../services/emailSender');
+const ejs = require("ejs");
 
 const organizationsService = require('../services/organizations');
 
 // Send a welcome mail to an email given
 async function sendWelcomeEmail(email, organizationId) {
   const organization = await organizationsService.getById(organizationId);
+  console.log(organization)
   let subject = 'Welcome to ' + organization.name;
   let text = subject;
   let html = ejs.render('welcomeEmail', { organization: this.organization });
