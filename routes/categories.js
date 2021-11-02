df577e4ee6205d9e9df3d708ce4267b0f075f85c
ivan gabriel achocalla <ivangabriel.2048@gmail.com>
2021-11-02 13:39:30 -0300

    adding 'middlewares' to the path

--- routes/categories.js
+++ routes/categories.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const categoriesRouter = Router();
 const categoriesController = require('../controllers/categories')
+const { validateCategoryDetails } = require('../middlewares/validateCategoryDetails')
+const { isAdmin } = require('../middlewares/auths')
 
-categoriesRouter.post('/', categoriesController.create);
+categoriesRouter.post('/', isAdmin, validateCategoryDetails, categoriesController.create);
 
 module.exports = categoriesRouter;
