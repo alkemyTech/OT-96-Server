8f563fc5197ca21435716c9e700bdd01d1f968ed
ivan gabriel achocalla <ivangabriel.2048@gmail.com>
2021-11-02 13:04:23 -0300

    modifications related to good practices

--- repositories/categories.js
+++ repositories/categories.js
@@ -1,50 +1,40 @@
-const { Category } = require('../models/category');
+const { Category } = require('../models');
 
-// Get all categories
 const getAll = async () => {
-    const response = await Category.findAll({
-        attributes: {
-            exclude: ['description', 'deletedAt', 'createdAt', 'updatedAt'],
-        }
-    });
-
-    return response;
+  const response = await Category.findAll({
+    attributes: {
+      exclude: ['description', 'deletedAt', 'createdAt', 'updatedAt'],
+    }
+  });
+  return response;
 }
 
-// Get one category by id
 const getById = async (id) => {
-    const response = await Category.findByPk(id);
-
-    return response;
+  const response = await Category.findByPk(id);
+  return response;
 }
 
-// Insert new category
 const create = async (data) => {
-    const response = await Category.create(data);
-
-    return response;
+  const response = await Category.create(data);
+  return response;
 }
 
-// Update category
 const update = async (id, data) => {
-    const response = await Category.update(data, {
-        where: {
-            id: id
-        }
-    });
-
-    return response;
+  const response = await Category.update(data, {
+    where: {
+      id
+    }
+  });
+  return response;
 }
 
-// Remove one category by id
 const remove = async (id) => {
-    const response = await Category.destroy({
-        where: {
-            id: id
-        }
-    });
-
-    return response;
+  const response = await Category.destroy({
+    where: {
+      id
+    }
+  });
+  return response;
 }
 
 module.exports = {
