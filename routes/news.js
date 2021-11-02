3c3bae51f4419a7a367ea6a5f2b0eb34025fc8e1
ivan gabriel achocalla <ivangabriel.2048@gmail.com>
2021-11-02 14:54:22 -0300

    add route new

---
+++ routes/news.js
@@ -0,0 +1,7 @@
+const { Router } = require('express');
+const newsRouter = Router();
+const newsController = require('../controllers/news')
+
+newsRouter.put('/:id', newsController.update);
+
+module.exports = newsRouter;
