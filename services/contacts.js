b2b8b8abe7cdac6614c18003ed01cdba76f43b2f
Matias Preiti <preitimatias@gmail.com>
2021-11-02 00:43:06 -0300

    file contacts services

---
+++ services/contacts.js
@@ -0,0 +1,21 @@
+const contactsRepository = require('../repositories/contacts');
+
+//  Example:
+/* const getAll = async (req, res, next) => {
+    try {
+        const response = await contactsRepository.getAll();
+        return response;
+    } catch (error) {
+        next(error);
+    }
+} 
+
+
+module.exports = {
+    // getAll,
+    // getById,
+    // create,
+    // update,
+    // remove
+}
+ */
