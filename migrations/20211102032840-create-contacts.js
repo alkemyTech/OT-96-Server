dfa94df197b3258de5b7afbd09de684d6ba77f6c
Matias Preiti <preitimatias@gmail.com>
2021-11-02 00:42:28 -0300

    Model+Migrations Contacts Created

---
+++ migrations/20211102032840-create-contacts.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Contacts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      phone: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      messege: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      deleteAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Contacts');
+  },
+};
