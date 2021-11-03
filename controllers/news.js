bbdf527c1e93ba3fd139c3f6576cee4c5e8f3abe
ivan gabriel achocalla <ivangabriel.2048@gmail.com>
2021-11-03 19:26:40 -0300

    function optimization

--- controllers/news.js
+++ controllers/news.js
@@ -2,9 +2,11 @@ const newsService = require("../services/news");
 
 const update = async (req, res, next) => {
   try {
-    const response = await newsService.update(req.body, req.params.id);
-    if (response[0] !== 1) {
-      const error = new Error("los parametros que mandaste no existen");
+    const [response] = await newsService.update(req.body, req.params.id);
+    if (!response) {
+      const error = new Error(
+        "ninguno de los parametros que mandaste coinciden"
+      );
       error.status = 409;
       throw error;
     }
