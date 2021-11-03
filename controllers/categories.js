799ad4908b025f8f950a4882ea5f0ecc6dae4649
Matias Preiti <preitimatias@gmail.com>
2021-11-03 00:07:24 -0300

    refactor

--- controllers/categories.js
+++ controllers/categories.js
@@ -3,7 +3,11 @@ const categoriesService = require('../services/categories');
 const update = async (req, res, next) => {
   try {
     const response = await categoriesService.update(req.params.id, req.body);
-    return res.status(200).json(response);
+    res.status(200).json({
+      success: true,
+      msg: `Category ${req.params.id} is updated succesfully`,
+      Category: response,
+    });
   } catch (error) {
     next(error);
   }
