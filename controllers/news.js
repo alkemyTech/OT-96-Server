ad152b331677cef24fa3a5254736451d219a3074
Federico Palmieri <fedepalmie95@gmail.com>
2021-11-03 01:02:46 -0300

    add newController

--- controllers/news.js
+++ controllers/news.js
@@ -1,25 +1,39 @@
 const newsService = require('../services/news');
 
 
-module.exports.getAll = async (req, res, next) => {
+const getAll = async (req, res, next) => {
 
 };
 
-module.exports.getById = async (req, res, next) => {
+const getById = async (req, res, next) => {
 
 };
 
-module.exports.create = async (req, res, next) => {
+const create = async (req, res, next) => {
 
 };
 
-module.exports.update = async (req, res, next) => {
+const update = async (req, res, next) => {
 
 };
 
-module.exports.delete = async (req, res, next) => {
-
+const remove = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        await newsService.remove(id);
+        res.status(200).json({ message: 'the news was delete succesfully!' });
+    } catch(error) {
+        next(error);
+    }
 };
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove,
+}
 
 
 
