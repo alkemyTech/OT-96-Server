738a08afb2292325bd0193001a8be43cf9dfdd70
Alexis Zacre <84038648+Aleex1237@users.noreply.github.com>
2021-11-02 21:06:55 -0300

    Ot96 138/listado usuarios (#35)
    * added route /users & middleware isAdmin
    
    * getAll users method
    
    * error handler for all Users
    
    * added right path to imports

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+const usersController = require('../controllers/users');
+const authMiddleware = require('../middlewares/auths');
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/',authMiddleware.isAdmin ,usersController.getAll);
 
 module.exports = router;
