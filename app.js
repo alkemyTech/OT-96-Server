a16c25bccb02d97f5437f7cdc731bf4e36032e13
juancruz_94_ <juancruz_94_@hotmail.com>
2021-11-03 19:19:47 -0300

    app.js categories router

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors')
-require('dotenv').config()
+const cors = require('cors');
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const authRouter = require('./routes/authentications')
+const authRouter = require('./routes/authentications');
 const organizationRouter = require('./routes/organization.js');
+const categoriesRouter = require('./routes/categories.js');
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,14 +29,15 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/organization', organizationRouter);
 app.use('/auth', authRouter);
+app.use('/categories', categoriesRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
