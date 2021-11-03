59ed287bfbae14a0fb6e3b8aecb2138aba39af19
ivan gabriel achocalla <ivangabriel.2048@gmail.com>
2021-11-03 19:00:52 -0300

    change of error handling according to good practices

--- controllers/news.js
+++ controllers/news.js
@@ -3,15 +3,10 @@ const newsService = require("../services/news");
 const update = async (req, res) => {
   try {
     const response = await newsService.update(req.body, req.params.id);
-    if (!response) {
-      return res
-        .status(400)
-        .json({ ok: false, msg: "la noticia con el id no existe" });
-    }
     if (response[0] !== 1) {
-      return res
-        .status(400)
-        .json({ ok: false, msg: "los parametros que mandaste no existen" });
+      const error = new Error("los parametros que mandaste no existen");
+      error.status = 409;
+      throw error;
     }
     return res.status(200).json({ ok: true });
   } catch (error) {
--- services/news.js
+++ services/news.js
@@ -14,7 +14,11 @@ const create = async ({ name, content, image }) => {
 
 const update = async ({ name, content, image }, id) => {
   const existNews = await newsRepository.getById(id);
-  if (!existNews) return false;
+  if (!existNews) {
+    const error = new Error("la noticia con el " + id + " no existe");
+    error.status = 409;
+    throw error;
+  }
   return await newsRepository.update({ name, content, image }, id);
 };
 
