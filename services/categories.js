a75cb6c08b2ee1ad08e3a9555651f66cbc0935c2
Aleex1237 <alexis10893@outlook.com>
2021-11-03 18:46:25 -0300

    added getById category method on services & error handler

--- services/categories.js
+++ services/categories.js
@@ -1,12 +1,22 @@
-const categoriesRepository = require('../repositories/categories');
+const categoriesRepository = require("../repositories/categories");
 
 //example:
-/**
- const getAll = async () => {
-        return  await categoriesRepository.getAll();
-    }
+const getAll = async () => {
+  return await categoriesRepository.getAll();
+};
+
+const getById = async (id) => {
+  const category = await categoriesRepository.getById(id);
+    
+  if (category) {
+    return category;
+  }
+  const error = new Error("No existe la categoria!");
+  error.status = 404;
+  throw error;
+};
 
 module.exports = {
-    getAll
-    }
- */
+  getAll,
+  getById,
+};
