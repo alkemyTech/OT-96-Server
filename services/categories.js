800698148b2bac29a5be625170e6e751f96ebe90
Matias Preiti <preitimatias@gmail.com>
2021-11-03 00:20:31 -0300

    refactor service

--- services/categories.js
+++ services/categories.js
@@ -1,14 +1,24 @@
 const categoriesRepository = require('../repositories/categories');
 
 const update = async (id, categoryBody) => {
-  const categoryResponse = await categoriesRepository.update(id, categoryBody);
-  if (!categoryResponse) {
+  try {
+    const categoryResponse = await categoriesRepository.update(
+      id,
+      categoryBody
+    );
+    if (!categoryResponse) {
+      return {
+        status: 404,
+        message: 'Category not found',
+      };
+    }
+    return categoryResponse;
+  } catch (error) {
     return {
-      status: 404,
-      message: 'Category not found',
+      status: 500,
+      message: 'Internal server error - Services',
     };
   }
-  return categoryResponse;
 };
 
 module.exports = {
