14b7a92a79456974bd272dab8eb6fae5960f066e
ivan gabriel achocalla <ivangabriel.2048@gmail.com>
2021-11-03 15:44:32 -0300

    repeating category validator

--- controllers/categories.js
+++ controllers/categories.js
@@ -3,6 +3,9 @@ const categoriesService = require("../services/categories");
 const create = async (req, res) => {
   try {
     const response = await categoriesService.create(req.body);
+    if (!response) {
+      res.status(400).json({ ok: false, msg: "categoria repetida" });
+    }
     return res.status(200).json(response);
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
--- services/categories.js
+++ services/categories.js
@@ -1,6 +1,8 @@
 const categoriesRepository = require("../repositories/categories");
 
 const create = async ({ name, image, description }) => {
+  const res = await categoriesRepository.getByName(name);
+  if (res) return false;
   return await categoriesRepository.create({ name, image, description });
 };
 
