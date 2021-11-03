12fb19df4509ff80b80dfc5d3b9e104af3166908
Matias Preiti <preitimatias@gmail.com>
2021-11-03 00:34:35 -0300

    news Repository

--- repositories/news.js
+++ repositories/news.js
@@ -1,48 +1,53 @@
 const newsModel = require('../models/news');
 
-module.exports.getAll = async ( ) => {
-    const response = await newsModel.findAll({
-        include: [
-            {
-                model: db.Category,
-            },
-        ],
-    });
-    return response;
-};
-
-module.exports.getById = async ( id ) => {
-    const response = await newsModel.findOne({
-        where: { id }, include: [{ model: newsModel.Category }],
-    });
-
-    return response;
-};
-
-module.exports.create = async ( data ) => {
-    const response = await newsModel.create({
-        name: data.name,
-        content: data.content,
-        image: data.image,
-        categoryId: data.categoryId
-    })
-
-    return response;
-}
-
-module.exports.update = async ( id, data ) => {
-    const response = await newsModel.update({
-        name: data.name,
-        content: data.content,
-        image: data.image,
-        categoryId: data.categoryId
+const getAll = async () => {
+  const response = await newsModel.findAll({
+    include: [
+      {
+        model: db.Category,
       },
-      { where: { id } });
-    
-    return response;
-}
+    ],
+  });
+  return response;
+};
 
-module.exports.remove = async ( id ) => {
-    const response = newsModel.destroy({ where: { id } });
-    return response;
-}
+const getById = async (id) => {
+  const response = await newsModel.findOne({
+    where: { id },
+    include: [{ model: newsModel.Category }],
+  });
+
+  return response;
+};
+
+const create = async (data) => {
+  const news = await newsModel.create(data);
+  return news;
+};
+
+const update = async (id, data) => {
+  const response = await newsModel.update(
+    {
+      name: data.name,
+      content: data.content,
+      image: data.image,
+      categoryId: data.categoryId,
+    },
+    { where: { id } }
+  );
+
+  return response;
+};
+
+const remove = async (id) => {
+  const response = newsModel.destroy({ where: { id } });
+  return response;
+};
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+  update,
+  remove,
+};
