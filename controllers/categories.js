bc3b8cf5bb42ba50adbfb4567b9e743edff850a6
ivan gabriel achocalla <ivangabriel.2048@gmail.com>
2021-11-03 18:49:59 -0300

    change of error handling according to good practices

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,14 +1,11 @@
 const categoriesService = require("../services/categories");
 
-const create = async (req, res) => {
+const create = async (req, res, next) => {
   try {
     const response = await categoriesService.create(req.body);
-    if (!response) {
-      res.status(400).json({ ok: false, msg: "categoria repetida" });
-    }
     return res.status(200).json(response);
   } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
+    next(error);
   }
 };
 
--- services/categories.js
+++ services/categories.js
@@ -2,7 +2,11 @@ const categoriesRepository = require("../repositories/categories");
 
 const create = async ({ name, image, description }) => {
   const res = await categoriesRepository.getByName(name);
-  if (res) return false;
+  if (res) {
+    const error = new Error("categoria repetida");
+    error.status = 409;
+    throw error;
+  }
   return await categoriesRepository.create({ name, image, description });
 };
 
