fc6f03db61efbb89c9f7884108c26c37446789ac
juancruz_94_ <juancruz_94_@hotmail.com>
2021-11-03 18:45:57 -0300

    controllers/categories remove

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,7 +1,7 @@
 const categoriesService = require("../services/categories");
 
 //example:
-/**
+/*
 const getAll = async (req, res, next) => {
     try {
         const response = await categoriesService.getAll();
@@ -10,9 +10,27 @@ const getAll = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-} 
+}
+*/
+
+const remove = async (req, res, next) => {
+  try {
+    const category = await categoriesService.getById(req.params.id);
+    if (category) {
+      await categoriesService.remove(req.params.id);
+      res.status(201).send("Categoría eliminada!");
+    } else {
+      const error = new Error(
+        `No existe la categoria con ID: ${req.params.id}!`
+      );
+      error.status = 404;
+      throw error;
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
-    getAll
-}
- */
+  remove,
+};
