17c365fb155b91281114d48e8903337856de9c1a
Francisco Olivero Sagasta <francisco.olivero1998@gmail.com>
2021-11-04 20:22:03 -0300

    Actualizacion de organizacion

--- routes/organization.js
+++ routes/organization.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
 const organizationRouter = Router();
 const organizationController = require('../controllers/organization.js')
+const { isAdmin } = require('../middlewares/auths')
+const { validateOrganization } = require('../middlewares/organizationValidation')
 
 
 organizationRouter.get('/:id/public', organizationController.getOrganizationPublic);
+organizationRouter.post('/:id/public',isAdmin, validateOrganization ,organizationController.update);
 
 module.exports = organizationRouter;
