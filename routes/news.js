a57a815f40bd24edf0c0296c9548526918b3ccb2
ivan gabriel achocalla <ivangabriel.2048@gmail.com>
2021-11-05 15:26:34 -0300

    bug fixed in middleware name

--- routes/news.js
+++ routes/news.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
 const newsRouter = Router();
 const newsController = require('../controllers/news');
-const { newsDataValidation } = require('../middlewares/newsDataValidation');
+const newsDataValidation = require('../middlewares/newsDataValidation');
 const { isAdmin } = require('../middlewares/auths');
 
 newsRouter.put('/:id', isAdmin, newsController.update);
 newsRouter.post('/', isAdmin, newsDataValidation, newsController.create);
-newsRouter.delete('/:id',[isAdmin], newsController.remove);
+newsRouter.delete('/:id', [isAdmin], newsController.remove);
 
 module.exports = newsRouter;
