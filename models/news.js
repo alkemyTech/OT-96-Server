b0c285b3303e39840081f273489b2e8d8950a4b6
Palmieri31 <49069975+Palmieri31@users.noreply.github.com>
2021-11-05 20:03:12 -0300

    fix alias in model category (#64)

--- models/news.js
+++ models/news.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      News.belongsTo(models.Category);
+      News.belongsTo(models.Category, { as: 'category' });
     }
   }
   News.init(
