be239f612fd703882859a06603108a6b775b8dd5
Agustin <agualvila@gmail.com>
2021-11-05 00:05:18 -0300

    add activities update services

--- services/activities.js
+++ services/activities.js
@@ -1,12 +1,28 @@
 const activitiesRepository = require('../repositories/activities');
 
-//example:
-/**
- const getAll = async () => {
-    return await activitiesRepository.getAll();
-}
+const create = async ({ name, content, image }) => {
+  const res = await activitiesRepository.getByName(name);
+  if (res) {
+    const error = new Error(`Activity ${name} already exists`);
+    error.status = 400;
+    throw error;
+  }
+  return await activitiesRepository.create({ name, content, image });
+};
+
+const update = async (id, { name, content, image }) => {
+  console.log(id, { name, content, image });
+
+  const res = await activitiesRepository.getById(id);
+  if (!res) {
+    const error = new Error(`Activity ${id} doesen't exists`);
+    error.status = 404;
+    throw error;
+  }
+  return await activitiesRepository.update(id, { name, content, image });
+};
 
 module.exports = {
-    getAll
-}
- */
+  create,
+  update,
+};
