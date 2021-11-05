ce78c9c4d27f1c1d657574a5d1b04740acb30ed4
ivan gabriel achocalla <ivangabriel.2048@gmail.com>
2021-11-05 13:55:38 -0300

    prettier

--- services/aws_s3.js
+++ services/aws_s3.js
@@ -3,43 +3,42 @@ const fs = require('fs');
 const AWS = require('aws-sdk');
 
 // AWS account data
-const bucketName = process.env.AWS_BUCKET
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID
-const sercetAccesKey = process.env.AWS_SECRET_ACCESS_KEY
-const region = process.env.AWS_REGION
+const bucketName = process.env.AWS_BUCKET;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const sercetAccesKey = process.env.AWS_SECRET_ACCESS_KEY;
+const region = process.env.AWS_REGION;
 
 // Create S3 instance
 const s3 = new AWS.S3({
-    region,
-    accessKeyId,
-    sercetAccesKey
+  region,
+  accessKeyId,
+  sercetAccesKey,
 });
-
 
 /** Upload file **************/
 const uploadFile = (file) => {
-    const fileStream = fs.createReadStream(file.path)
+  const fileStream = fs.createReadStream(file.path);
 
-    const uploadParams = {
-        Bucket: bucketName,
-        Body: fileStream,
-        Key: file.filename
-    }
+  const uploadParams = {
+    Bucket: bucketName,
+    Body: fileStream,
+    Key: file.filename,
+  };
 
-    return s3.upload(uploadParams).promise();
-}
+  return s3.upload(uploadParams).promise();
+};
 
 /** Download file **************/
 const getFileStream = (key) => {
-    const downloadParams = {
-        Key: key,
-        Bucket: bucketName
-    }
+  const downloadParams = {
+    Key: key,
+    Bucket: bucketName,
+  };
 
-    return s3.getObject(downloadParams).createReadStream();
-}
+  return s3.getObject(downloadParams).createReadStream();
+};
 
 module.exports = {
-    uploadFile,
-    getFileStream
-}
+  uploadFile,
+  getFileStream,
+};
