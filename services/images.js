f08b632e3672dc487ed827ff4450798c7ed5f6ef
ivan gabriel achocalla <ivangabriel.2048@gmail.com>
2021-11-05 15:14:35 -0300

    creacion de servicio image

---
+++ services/images.js
@@ -0,0 +1,19 @@
+const awsServices = require('../services/aws_s3');
+
+const create = async (file) => {
+  try {
+    if (!file) {
+      const error = new Error(`el archivo no existe`);
+      error.status = 404;
+      throw error;
+    }
+    const response = await awsServices.uploadFile(file);
+    return response.Bucket;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  create,
+};
