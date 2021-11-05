857b9e671f4d6648a7db947ddacbae340f975e10
Matias Preiti <preitimatias@gmail.com>
2021-11-04 22:47:30 -0300

    middleware de validación

---
+++ middlewares/activityDataValidation.js
@@ -0,0 +1,31 @@
+const { check, validationResult } = require('express-validator');
+
+const activitiDataValidation = [
+  check('name')
+    .notEmpty()
+    .withMessage('You need to enter a name!')
+    .bail()
+    .isAlphanumeric()
+    .withMessage('Invalid Name')
+    .bail(),
+
+  check('content')
+    .notEmpty()
+    .withMessage('You need to enter a content!')
+    .bail()
+    .isAlphanumeric()
+    .withMessage('the content fail')
+    .bail(),
+
+  check('image').notEmpty().withMessage('You need to enter a image!').bail(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
+
+module.exports = { activitiDataValidation };
