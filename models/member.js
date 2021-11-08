3948a1c0918e3a18f1e96f1e60f86f6f375ea604
Agustin <agualvila@gmail.com>
2021-11-08 17:35:13 -0300

    fix member model

---
+++ models/member.js
@@ -0,0 +1,30 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Member extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Member.init(
+    {
+      name: DataTypes.STRING,
+      facebookUrl: DataTypes.STRING,
+      instagramUrl: DataTypes.STRING,
+      linkedinUrl: DataTypes.STRING,
+      image: DataTypes.STRING,
+      deletedAt: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: 'Member',
+      paranoid: true,
+    }
+  );
+  return Member;
+};
