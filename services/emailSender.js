528743fafacff9df4e28f4713ebaaddf87bdb5aa
Aleex1237 <alexis10893@outlook.com>
2021-11-08 21:20:12 -0300

    fix emailTemplate

--- services/emailSender.js
+++ services/emailSender.js
@@ -1,21 +1,20 @@
 const sendgridMail = require("@sendgrid/mail");
 sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
-const emailSender = async (email, subject, text, html) => {
+
+const send = async (email, html) => {
   try {
     const msg = {
       to: email,
-      from: "juancruzalmazan1994@gmail.com",
-      subject: subject,
-      text: text,
+      from: "fundacionsomosmas@outlook.com.ar",
+      subject: "Welcome Email",
       html: html,
     };
 
-    const result = await sendgridMail.send(msg);
-    console.log(result);
+    await sendgridMail.send(msg);
     console.log({ msg: "Email sent successfully!" });
   } catch (error) {
     console.error({ error: error, msg: "Email sent failed" });
   }
 };
 
-module.exports = { emailSender };
+module.exports = { send };
