0b7cfa16ffdf0eef18ae8cdbb00f3065439c5daa
ivan gabriel achocalla <ivangabriel.2048@gmail.com>
2021-11-09 20:22:38 -0300

    creating a new middleware

---
+++ middlewares/users.js
@@ -0,0 +1,67 @@
+const { check, validationResult } = require('express-validator');
+
+const errorHandler = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  next();
+};
+
+const validateLogin = [
+  check('email')
+    .notEmpty()
+    .withMessage('You need to enter an email!')
+    .bail()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage('Invalid email. Ej:name@mail.com')
+    .bail(),
+
+  check('password')
+    .notEmpty()
+    .withMessage('You need to enter a password!')
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage('The password must be at least 6 characters long')
+    .bail(),
+  errorHandler
+];
+
+const validateUser = [
+  check('email')
+    .notEmpty()
+    .withMessage('You need to enter an email!')
+    .bail()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage('Invalid email. Ej:name@mail.com')
+    .bail(),
+
+  check('lastName')
+    .notEmpty()
+    .withMessage('You need to enter a last name!')
+    .bail()
+    .isAlpha()
+    .withMessage('LastName can only contain letters')
+    .bail(),
+
+  check('firstName')
+    .notEmpty()
+    .withMessage('You need to enter a first name!')
+    .bail()
+    .isAlpha()
+    .withMessage('FirstName can only contain letters')
+    .bail(),
+
+  check('password')
+    .notEmpty()
+    .withMessage('You need to enter a password!')
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage('The password must be at least 6 characters long')
+    .bail(),
+  errorHandler
+];
+
+module.exports = { validateUser, validateLogin };
