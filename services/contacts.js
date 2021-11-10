a58f06fad0db5c6399987f1c1bb6f029fec12611
Aleex1237 <alexis10893@outlook.com>
2021-11-10 17:31:05 -0300

    contact service has been uncommented

--- services/contacts.js
+++ services/contacts.js
@@ -1,7 +1,7 @@
 const contactsRepository = require('../repositories/contacts');
 
 //  Example:
-/* const getAll = async (req, res, next) => {
+const getAll = async (req, res, next) => {
   const response = await contactsRepository.getAll();
   if (response.length == 0) {
     const error = new Error(`No hay contactos!.`);
@@ -12,5 +12,5 @@ const contactsRepository = require('../repositories/contacts');
 };
 
 module.exports = {
-  // getAll
-}; */
+  getAll
+};
