f7e9195dce2400ae4aaabad0c1b6ccd0db3ae0e8
ivan gabriel achocalla <ivangabriel.2048@gmail.com>
2021-11-09 21:00:12 -0300

    update path organizations

--- routes/organization.js
+++ routes/organization.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const organizationRouter = express.Router();
-const organizationController = require('../controllers/organization');
+const organizationsController = require('../controllers/organization');
 const authMiddleware = require('../middlewares/auths');
-const validateOrganization = require('../middlewares/organizations');
+const organizationsMiddleware = require('../middlewares/organizations');
 
 organizationRouter.get(
   '/:id/public',
-  organizationController.getOrganizationPublic
+  organizationsController.getOrganizationPublic
 );
 organizationRouter.post(
   '/:id/public',
   authMiddleware.isAdmin,
-  validateOrganization,
-  organizationController.update
+  organizationsMiddleware.validateOrganization,
+  organizationsController.update
 );
 
 module.exports = organizationRouter;
