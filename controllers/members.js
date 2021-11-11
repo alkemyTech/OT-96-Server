5265d237bd9f35a73bb9a9f61231d4517ff0afd9
Agustín Avila Humerez <agualvila@gmail.com>
2021-11-11 11:28:57 -0300

    Ot96 176 - Endpoint Creación miembros (#79)
    - Add member create repository
    - Add member create service
    - Add member create controller
    - Add member create route
    - Add member middleware

--- controllers/members.js
+++ controllers/members.js
@@ -9,6 +9,20 @@ const getAll = async (req, res, next) => {
   }
 };
 
+const create = async (req, res, next) => {
+  try {
+    const response = await membersService.create(req.body);
+    res.status(200).json({
+      success: true,
+      msg: `member: ${response.name} has been created`,
+      member: response
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
-  getAll
+  getAll,
+  create
 };
