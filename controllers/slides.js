a186a53dc8b960b57ed660b1d20acedfef0fbde3
ivan gabriel achocalla <ivangabriel.2048@gmail.com>
2021-11-10 21:07:02 -0300

    espacio quitado

--- controllers/slides.js
+++ controllers/slides.js
@@ -12,7 +12,6 @@ const getAll = async (req, res, next) => {
 const getById = async (req, res, next) => {
   try {
     const requestSlide = await SlidesService.getById(req.params.id);
-
     res.send(requestSlide);
   } catch (error) {
     next(error);
@@ -25,7 +24,7 @@ const create = async (req, res, next) => {
     res.status(200).json({
       success: true,
       msg: `your Slide ${newSlide.title} has been created`,
-      Slide: newSlide,
+      Slide: newSlide
     });
   } catch (error) {
     next(error);
@@ -38,7 +37,7 @@ const update = async (req, res, next) => {
     res.status(200).json({
       success: true,
       msg: `Slide ${req.params.id} is updated succesfully`,
-      Slide: updateSlide,
+      Slide: updateSlide
     });
   } catch (error) {
     next(error);
@@ -50,7 +49,7 @@ const remove = async (req, res, next) => {
     const softDeleteSlide = await SlidesService.remove(req.params.id);
     res.status(201).json({
       success: true,
-      msg: `your Slide ${req.body.name} has been deleted`,
+      msg: `your Slide ${req.body.name} has been deleted`
     });
   } catch (error) {
     next(error);
