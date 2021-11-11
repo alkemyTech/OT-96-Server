36c57677f139cc48fad9b5f1bd2fa63587235f2a
Federico Palmieri <fedepalmie95@gmail.com>
2021-11-11 13:06:34 -0300

    update member

--- controllers/members.js
+++ controllers/members.js
@@ -22,7 +22,21 @@ const create = async (req, res, next) => {
   }
 };
 
+const update = async (req, res, next) => {
+  try {
+    const response = await membersService.update(req.params.id, req.body);
+    res.status(200).json({
+      success: true,
+      msg: `member ${req.params.id} is updated succesfully`,
+      member: response
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getAll,
-  create
+  create,
+  update
 };
