925d806622a1862d9e96dbb921d4fa20fb07cce3
Matias Preiti <preitimatias@gmail.com>
2021-11-11 01:25:38 -0300

    controllers

---
+++ controllers/comments.js
@@ -0,0 +1,14 @@
+const commentsServices = require('../services/comments');
+
+/* const getAll = async (req, res, next) => {
+  try {
+    const response = await commentsServices.getAll();
+    return res.status(200).json({ data: response });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getAll
+}; */
