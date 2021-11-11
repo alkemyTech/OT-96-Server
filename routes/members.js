78bbc62d97b2c92afa88fd5ed972ec9a1fcfd21a
Agustin <agualvila@gmail.com>
2021-11-10 22:39:58 -0300

    add members create route

--- routes/members.js
+++ routes/members.js
@@ -2,11 +2,15 @@ const express = require('express');
 const membersRouter = express.Router();
 const membersControllers = require('../controllers/members');
 const authMiddleware = require('../middlewares/auths');
+const membersMiddleware = require('../middlewares/members');
 
-membersRouter.get(
-  '/', 
-  authMiddleware.isAdmin,
-  membersControllers.getAll
+membersRouter.get('/', authMiddleware.isAdmin, membersControllers.getAll);
+
+membersRouter.post(
+  '/',
+  authMiddleware.isLoggedUser,
+  membersMiddleware.validateMember,
+  membersControllers.create
 );
 
 module.exports = membersRouter;
