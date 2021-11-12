c417c5891fe72d79774c8cf8bdacaad56fd54d8b
gabr13l <52333147+Igabri3l@users.noreply.github.com>
2021-11-11 22:57:31 -0300

    Ot96 161/creacion slides (#78)
    * send files to aws
    
    * change of data type in slide model
    
    * added order feature
    
    * espacio quitado
    
    * key added to request
    
    * added param text

--- controllers/slides.js
+++ controllers/slides.js
@@ -12,7 +12,6 @@ const getAll = async (req, res, next) => {
 const getById = async (req, res, next) => {
   try {
     const requestSlide = await SlidesService.getById(req.params.id);
-
     res.send(requestSlide);
   } catch (error) {
     next(error);
@@ -21,11 +20,12 @@ const getById = async (req, res, next) => {
 
 const create = async (req, res, next) => {
   try {
-    const newSlide = await SlidesService.create(req.body);
+    const { newSlide, key } = await SlidesService.create(req.body);
     res.status(200).json({
       success: true,
-      msg: `your Slide ${newSlide.title} has been created`,
-      Slide: newSlide,
+      msg: `your Slide ${newSlide.text} has been created`,
+      key,
+      Slide: newSlide
     });
   } catch (error) {
     next(error);
@@ -38,7 +38,7 @@ const update = async (req, res, next) => {
     res.status(200).json({
       success: true,
       msg: `Slide ${req.params.id} is updated succesfully`,
-      Slide: updateSlide,
+      Slide: updateSlide
     });
   } catch (error) {
     next(error);
@@ -50,7 +50,7 @@ const remove = async (req, res, next) => {
     const softDeleteSlide = await SlidesService.remove(req.params.id);
     res.status(201).json({
       success: true,
-      msg: `your Slide ${req.body.name} has been deleted`,
+      msg: `your Slide ${req.body.name} has been deleted`
     });
   } catch (error) {
     next(error);
