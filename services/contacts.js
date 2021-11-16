330ca229a65d63fbaf8b3bd7a22be2e562788098
Matias Preiti <preitimatias@gmail.com>
2021-11-16 00:34:59 -0300

    normalización respuesta GetAll

--- services/contacts.js
+++ services/contacts.js
@@ -4,28 +4,24 @@ const contactEmail = require('../services/addContactEmail');
 //  Example:
 const getAll = async (req, res, next) => {
   const response = await contactsRepository.getAll();
-  if (response.length == 0) {
-    const error = new Error(`No hay contactos!.`);
-    error.status = 404;
-  }
   return response;
 };
 const create = async ({ name, phone, email, message }) => {
-	const response = await contactsRepository.create({
-		name,
-		phone,
-		email,
-		message
-	});
+  const response = await contactsRepository.create({
+    name,
+    phone,
+    email,
+    message
+  });
 
-	if (!response) {
-		const error = new Error('there was an error in creation of contact');
-		error.status = 403;
-		throw error;
-	}
+  if (!response) {
+    const error = new Error('there was an error in creation of contact');
+    error.status = 403;
+    throw error;
+  }
 
-	await contactEmail.send(response.email, response.name);
-	return response;
+  await contactEmail.send(response.email, response.name);
+  return response;
 };
 
 module.exports = { create, getAll };
