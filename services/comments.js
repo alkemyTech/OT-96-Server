aa22b4428868349bf3bb4432742d72a89f0d829d
gabr13l <ivangabriel.2048@gmail.com>
2021-11-17 11:39:45 -0300

    added update method to services

--- services/comments.js
+++ services/comments.js
@@ -5,6 +5,20 @@ const getAll = async () => {
   return response;
 };
 
+const update = async (id, { body }) => {
+  const comments = await commentsRepository.getById(id);
+  console.log(comments);
+  if (!comments) {
+    const error = new Error('comments no encontrado.');
+    error.status = 409;
+    throw error;
+  }
+  await commentsRepository.update({ body }, id);
+
+  return await commentsRepository.getById(id);
+};
+
 module.exports = {
-  getAll
+  getAll,
+  update
 };
