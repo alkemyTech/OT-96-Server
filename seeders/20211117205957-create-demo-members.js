bcf5b3ff7496a5af59359972c99ca06eb0a1488c
Alexis Zacre <84038648+Aleex1237@users.noreply.github.com>
2021-11-18 14:09:42 -0300

    Ot96 109/paginacion members (#101)
    * added response for pagination
    
    * added logic for pagination of members in services
    
    * added limit, offset & parameters
    
    * seeders of members

---
+++ seeders/20211117205957-create-demo-members.js
@@ -0,0 +1,83 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    let members = [
+      {
+        name: `Manuel`,
+        facebookUrl: 'http://manuel.com',
+        instagramUrl: 'http://manuel.com',
+        linkedinUrl: 'http://manuel.com',
+        image: `Manuelimage`,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: `Alexis`,
+        facebookUrl: 'http://alexis.com',
+        instagramUrl: 'http://alexis.com',
+        linkedinUrl: 'http://alexis.com',
+        image: `image`,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: `Francisco`,
+        facebookUrl: 'http://francisco.com',
+        instagramUrl: 'http://francisco.com',
+        linkedinUrl: 'http://francisco.com',
+        image: `image`,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: `Federico`,
+        facebookUrl: 'http://federico.com',
+        instagramUrl: 'http://federico.com',
+        linkedinUrl: 'http://federico.com',
+        image: `image`,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: `Juan Cruz`,
+        facebookUrl: 'http://JuanCruz.com',
+        instagramUrl: 'http://JuanCruz.com',
+        linkedinUrl: 'http://JuanCruz.com',
+        image: `image`,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: `Agustin`,
+        facebookUrl: 'http://Agustin.com',
+        instagramUrl: 'http://Agustin.com',
+        linkedinUrl: 'http://Agustin.com',
+        image: `image`,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: `Ivan Gabriel`,
+        facebookUrl: 'http://IvanGabriel.com',
+        instagramUrl: 'http://IvanGabriel.com',
+        linkedinUrl: 'http://IvanGabriel.com',
+        image: `image`,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: `Matias`,
+        facebookUrl: 'http://Matias.com',
+        instagramUrl: 'http://Matias.com',
+        linkedinUrl: 'http://Matias.com',
+        image: `image`,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ];
+    await queryInterface.bulkInsert('Members', members, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {}
+};
