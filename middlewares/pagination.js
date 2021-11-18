4d446fd4a3501dddfccf287bb34759fd01c74221
Aleex1237 <alexis10893@outlook.com>
2021-11-18 13:58:37 -0300

    validation if req.query.page does not exist

--- middlewares/pagination.js
+++ middlewares/pagination.js
@@ -1,4 +1,10 @@
 const validate = (req, res, next) => {
+  if(!req.query.page){
+    const error = new Error("parameter page does not exist");
+    error.status = 404;
+    throw error;
+  }
+  
   if (req.query.page == 0) {
     const error = new Error("Page 0 does not exist");
     error.status = 404;
