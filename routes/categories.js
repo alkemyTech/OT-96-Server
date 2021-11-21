280ef5b62e323f6a8982d9db4c194a098c4620f6
gabr13l <52333147+Igabri3l@users.noreply.github.com>
2021-11-21 16:50:47 -0300

    category documentation (#114)

--- routes/categories.js
+++ routes/categories.js
@@ -5,6 +5,65 @@ const categoriesController = require('../controllers/categories');
 
 const categoriesMiddleware = require('../middlewares/categories');
 
+/**
+ *@swagger
+ *  {
+ *    "components": {
+ *      "schemas": {
+ *        "Categories": {
+ *          "type": "object",
+ *          "properties": {
+ *            "name": {
+ *              "type": "string",
+ *              "description": "The name of the category"
+ *             },
+ *            "image": {
+ *              "type": "string",
+ *              "description": "The image of the category"
+ *            },
+ *            "description": {
+ *              "type": "string",
+ *              "description": "The description of the category"
+ *            }
+ *          },
+ *         "required": [ "name" ],
+ *          "example": {
+ *            "name": "Category 1",
+ *            "image": "image url",
+ *            "description": "This is the content of the category"
+ *          }
+ *       }
+ *     }
+ *   }
+ *}
+ */
+
+/**
+ * @swagger
+ * "/categories": {
+ *    "post": {
+ *      "summary": "Create a new category",
+ *      "tags": ["Categories"],
+ *      "security":[{"token":[]}],
+ *      "requestBody": {
+ *        "required": true,
+ *        "content": {
+ *           "application/json": {
+ *              "schema": {
+ *                "type": "object",
+ *                "$ref": "#/components/schemas/Categories"
+ *                }
+ *            }
+ *         }
+ *      },
+ *      "responses": {
+ *        "200": { "description": "category has been created" },
+ *        "500": { "description": "Internal server error" }
+ *      }
+ *    }
+ * }
+ */
+
 categoriesRouter.post(
   '/',
   authMiddleware.isAdmin,
@@ -12,22 +71,135 @@ categoriesRouter.post(
   categoriesController.create
 );
 
+/**
+ * @swagger
+ * "/categories": {
+ *    "get": {
+ *      "summary": "list of all categories",
+ *      "tags": ["Categories"],
+ *      "responses": {
+ *        "200":{"description":"succesful"},
+ *        "content": {
+ *            "application/json": {
+ *              "schema": {
+ *                "type": "object",
+ *                "items": {
+ *                  "$ref": "#/components/schemas/Categories"
+ *                 }
+ *               }
+ *          }
+ *      },
+ *     "404":{"description":"Category not found"},
+ *     "500":{"description":"Internal Server Error"}
+ *     },
+ *     "security":[{"token":[]}]
+ *    }
+ * }
+ */
+
 categoriesRouter.get(
   '/',
   authMiddleware.isAdmin,
   categoriesController.getAllNames
 );
+
+/**
+ * @swagger
+ * "/categories/{id}": {
+ *    "get": {
+ *      "summary": "get a category by Id",
+ *      "tags": ["Categories"],
+ *      "security":[{"token":[]}],
+ *      "parameters": [
+ *               {
+ *                 "name": "id",
+ *                 "in": "path",
+ *                 "description": "The id of the news",
+ *                 "required": true,
+ *                 "type": "integer",
+ *                 "example" :1
+ *               }
+ *             ],
+ *      "responses": {
+ *        "200": { "description": "succesful" },
+ *        "500": { "description": "Internal server error" }
+ *      }
+ *    }
+ * }
+ */
+
 categoriesRouter.get(
   '/:id',
   authMiddleware.isAdmin,
   categoriesController.getById
 );
 
+/**
+ * @swagger
+ * "/categories/{id}": {
+ *    "put": {
+ *      "summary": "update a category by Id",
+ *      "tags": ["Categories"],
+ *      "security":[{"token":[]}],
+ *      "parameters": [
+ *               {
+ *                 "name": "id",
+ *                 "in": "path",
+ *                 "description": "The id of the category",
+ *                 "required": true,
+ *                 "type": "integer",
+ *                 "example" :1
+ *               }
+ *             ],
+ *      "requestBody": {
+ *        "required": true,
+ *        "content": {
+ *           "application/json": {
+ *              "schema": {
+ *                "type": "object",
+ *                "$ref": "#/components/schemas/Categories"
+ *                }
+ *            }
+ *         }
+ *      },
+ *      "responses": {
+ *        "200": { "description": "Category has been updated" },
+ *        "500": { "description": "Internal server error" }
+ *      }
+ *    }
+ * }
+ */
+
 categoriesRouter.put(
   '/:id',
   authMiddleware.isAdmin,
   categoriesController.update
 );
+
+/**
+ * @swagger
+ * "/categories/{id}": {
+ *    "delete": {
+ *      "summary": "remove a category by Id",
+ *      "tags": ["Categories"],
+ *      "security":[{"token":[]}],
+ *      "parameters": [
+ *               {
+ *                 "name": "id",
+ *                 "in": "path",
+ *                 "description": "The id of the category",
+ *                 "required": true,
+ *                 "type": "integer",
+ *                 "example" :1
+ *               }
+ *             ],
+ *      "responses": {
+ *        "200": { "description": "Category has been deleted" },
+ *        "500": { "description": "Internal server error" }
+ *      }
+ *    }
+ * }
+ */
 
 categoriesRouter.delete(
   '/:id',
