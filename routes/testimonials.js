404f9b1d2949445b91e1fcce0a16bc8623c8ad46
Agustín Avila Humerez <agualvila@gmail.com>
2021-11-21 15:36:36 -0300

    Ot96 190 / pagination testimonials (#104)
    * pagination middleware
    
    * testimonials pagination

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -3,6 +3,7 @@ const testimonialsRouter = express.Router();
 const testimonialsController = require('../controllers/testimonials');
 const authMiddleware = require('../middlewares/auths');
 const testimonialsMiddleware = require('../middlewares/testimonials.js');
+const pagination = require('../middlewares/pagination');
 
 testimonialsRouter.delete(
   '/:id',
@@ -19,6 +20,13 @@ testimonialsRouter.post(
   authMiddleware.isAdmin,
   testimonialsMiddleware.validateTestimonial,
   testimonialsController.create
+);
+
+testimonialsRouter.get(
+  '/',
+  authMiddleware.isAdmin,
+  pagination.validate,
+  testimonialsController.getAll
 );
 
 module.exports = testimonialsRouter;
