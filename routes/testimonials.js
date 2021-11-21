ee4a9aa7002e83e662cdb2dd68d21671d5bc4a08
Agustín Avila Humerez <agualvila@gmail.com>
2021-11-21 15:45:56 -0300

    add testimonials documentation (#112)
    - Added documentation to testimonials

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -5,16 +5,155 @@ const authMiddleware = require('../middlewares/auths');
 const testimonialsMiddleware = require('../middlewares/testimonials.js');
 const pagination = require('../middlewares/pagination');
 
+/**
+ *@swagger
+ *  {
+ *    "components": {
+ *      "schemas": {
+ *        "Testimonial": {
+ *          "type": "object",
+ *          "properties": {
+ *            "name": {
+ *              "type": "string",
+ *              "description": "The name of the testimonial"
+ *             },
+ *            "image": {
+ *              "type": "string",
+ *              "description": "The url of the testimonial image"
+ *            },
+ *            "content": {
+ *              "type": "string",
+ *              "description": "The content of the testimonial"
+ *            },
+ *          },
+ *         "required": [  "name", "content"],
+ *          "example": {
+ *            "name": "testimonial 1",
+ *            "image": "www.testimonial1image.com/image1.png",
+ *            "content": "This is the content of the testimonial.",
+ *          }
+ *       }
+ *     }
+ *   }
+ *}
+ */
+
+/**
+ * @swagger
+ *{
+ *    "/testimonials/{id}": {
+ *       "delete": {
+ *             "summary": "Delete a testimonial by id",
+ *             "parameters": [
+ *               {
+ *                 "name": "id",
+ *                 "in": "path",
+ *                 "description": "The id of the testimonial",
+ *                 "required": true,
+ *                 "type": "integer"
+ *              }
+ *             ],
+ *             "tags": [ "Testimonials" ],
+ *             "security":[{"token":[]}],
+ *             "responses": {
+ *                 "200": {
+ *                    "description": "The testimonial has been deleted"
+ *                 },
+ *                 "404": {
+ *                   "description": "Testimonial not found"
+ *                 },
+ *                 "500": {
+ *                   "description": "Internal server error"
+ *                 }
+ *              }
+ *          }
+ *      }
+ *   }
+ */
 testimonialsRouter.delete(
   '/:id',
   authMiddleware.isAdmin,
   testimonialsController.remove
 );
+
+/**
+ * @swagger
+ *{
+ *    "/testimonials/{id}": {
+ *       "put": {
+ *             "summary": "Update a testimonial by id",
+ *             "parameters": [
+ *               {
+ *                 "name": "id",
+ *                 "in": "path",
+ *                 "description": "The id of the testimonial",
+ *                 "required": true,
+ *                 "type": "integer"
+ *               },
+ *               {
+ *                 "name": "body",
+ *                 "in": "body",
+ *                 "description": "The testimonial to update",
+ *                 "required": true,
+ *                 "schema": {
+ *                     "$ref": "#/components/schemas/Testimonial"
+ *                  }
+ *               }
+ *             ],
+ *             "tags": [ "Testimonials" ],
+ *             "security":[{"token":[]}],
+ *             "responses": {
+ *                 "200": {
+ *                    "description": "The testimonial has been updated"
+ *                 },
+ *                 "404": {
+ *                   "description": "Testimonial not found"
+ *                 },
+ *                 "500": {
+ *                   "description": "Internal server error"
+ *                 }
+ *              }
+ *          }
+ *      }
+ *   }
+ */
 testimonialsRouter.put(
   '/:id',
   authMiddleware.isAdmin,
   testimonialsController.update
 );
+
+/**
+ * @swagger
+ *{
+ *    "/testimonials": {
+ *       "post": {
+ *             "summary": "Create a testimonial",
+ *             "parameters": [
+ *                {
+ *                   "name": "body",
+ *                   "in": "body",
+ *                   "description": "The testimonial to create",
+ *                   "required": true,
+ *                   "schema": {
+ *                     "$ref": "#/components/schemas/Testimonial"
+ *                    }
+ *                }
+ *             ],
+ *             "tags": [ "Testimonials" ],
+ *            "security":[{"token":[]}],
+ *             "responses": {
+ *                 "200": {
+ *                    "description": "The testimonial has been created"
+ *                 },
+ *                 "500": {
+ *                   "description": "Internal server error"
+ *                 }
+ *              }
+ *          }
+ *      }
+ *   }
+ */
 testimonialsRouter.post(
   '/',
   authMiddleware.isAdmin,
