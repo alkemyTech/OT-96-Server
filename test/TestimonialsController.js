f02aa1eaa325f9284be7b3e595486bc09e7d3a42
Agustin <agualvila@gmail.com>
2021-11-23 22:24:29 -0300

    workInProgres but notWorkingYet

---
+++ test/TestimonialsController.js
@@ -0,0 +1,103 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const testimonialsController = require('../controllers/testimonials');
+const testimonialsService = require('../services/testimonials');
+
+let mockedTestimonialsService;
+let res, stubStatus, spyJson, spyNext, req;
+
+describe('test testimonialsControllers', () => {
+  const expectedName = 'testimonial test';
+  const expectedContent = 'test testimonial content';
+  const expectedImage = 'testimonial-test.jpg';
+  const expectedId = 1;
+  const req = {
+    params: {
+      id: expectedId
+    },
+    body: {
+      name: expectedName,
+      content: expectedContent,
+      image: expectedImage
+    },
+    query: {
+      page: 1
+    }
+  };
+  const expectedTestimonial = {
+    id: expectedId,
+    name: expectedName,
+    content: expectedContent,
+    image: expectedImage
+  };
+
+  beforeEach(() => {
+    beforeEach(() => {
+      mockedTestimonialsService = sinon.mock(testimonialsService);
+
+      stubStatus = sinon.stub();
+      spyJson = sinon.spy();
+      stubStatus.returns({ json: spyJson });
+
+      spyNext = sinon.spy();
+
+      res = { status: stubStatus };
+    });
+    afterEach(() => {
+      mockedTestimonialsService.verify();
+    });
+  });
+
+  it('get testimonial by id', async () => {
+    const expectedJsonResult = {
+      status: 200,
+      data: expectedTestimonial
+    };
+
+    mockedTestimonialsService
+      .expects('getById')
+      .withExactArgs(req.params.id)
+      .resolves(expectedTestimonial);
+    await testimonialsController.getById(req, res, spyNext);
+
+    chai.assert.equal(
+      res.status.calledOnce,
+      true,
+      'asserts status is called 1 time'
+    );
+    chai.assert.equal(
+      res.status.args[0],
+      200,
+      'asserts status parameter is 200'
+    );
+    chai.assert.equal(
+      spyJson.calledOnce,
+      true,
+      'asserts json is called 1 time'
+    );
+    chai.assert.isArray(spyJson.args[0], 'is an array');
+    chai.assert.isObject(spyJson.args[0][0], 'is an object');
+    chai.assert.equal(spyJson.args[0][0].status, expectedJsonResult.status);
+    chai.assert.equal(
+      spyJson.args[0][0].testimonial.id,
+      expectedJsonResult.testimonial.id
+    );
+    chai.assert.equal(
+      spyJson.args[0][0].testimonial.name,
+      expectedJsonResult.testimonial.name
+    );
+  });
+  it('Testimonials getById Error', async () => {
+    const expectedError = new Error('test error');
+
+    mockedTestimonialsService
+      .expects('getById')
+      .withExactArgs(req.params.id)
+      .rejects(expectedError);
+
+    await testimonialsController.getById(req, res, spyNext);
+
+    chai.assert.equal(spyNext.calledOnce, true);
+    chai.assert.equal(spyNext.args[0][0], expectedError);
+  });
+});
